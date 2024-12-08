d6674dd8c20d8f9e49a4f2c129583a075157d113
Tanveer2020 <tanveerali.abasyn@gmail.com>
2024-12-08 15:06:48 +0500

    Initial commit

---
+++ app/Helpers/helper.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\Category;
+use App\Models\ProductImage;
+
+function getCategories() {
+
+	return Category::orderBy('name','ASC')
+	   ->with('sub_category')
+	   ->orderBy('id','DESC') 
+	   ->where('showHome', 'Yes')
+	   ->where('status',1)
+	   ->get();
+
+function getProductImage($productId) {
+	
+	return ProductImage::where('product_id',$productId)->first();
+}
+
+}
+
+?>
